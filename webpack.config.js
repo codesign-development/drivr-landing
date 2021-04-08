0988d8459e7145afffecb84b4fbcfe5827e1838b
MandujanoJoseLuis <jose.luis@codesign.mx>
2021-04-08 16:36:19 -0500

    SetUp Inicial

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HTMLWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename:'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ],
+      },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+
+}
