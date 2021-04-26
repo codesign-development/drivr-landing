687a71594bb4fb791a0974d9e0a39ac15844591b
MandujanoJoseLuis <jose.luis@codesign.mx>
2021-04-26 17:11:24 -0500

    merge

--- src/components/Pricing/Pricing.js
+++
@@ -1,9 +0,0 @@
-import Pricing from './PricingView.html'
-
-export default () => {
-    
-    const divElement = document.createElement('div')
-    divElement.innerHTML = Pricing
-
-    return divElement
-}
